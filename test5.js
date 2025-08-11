cb2a4c137c7342ae0f2e49086dbdcaacb694d4a8
unknown <danishbinqayyum123@gmail.com>
2025-08-11 21:32:09 +0500

    09/8/2025

--- test5.js
+++ test5.js
@@ -33,18 +33,15 @@ const people = [
 
 //Q 1 )Write a code using map() to convert all city names in the array ['New York', 'London', 'Paris'] to uppercase.
 const  ar = ['new York', 'london', 'paris']
-let f1 = ar.map((e)=>e.toUpperCase())
-console.log(f1);//['NEW YORK','LONDON','PAEIS']
+console.log(ar.map((e)=>e.toUpperCase()));//['NEW YORK','LONDON','PAEIS']
 
 //Q 2 )Create a map() function to get the length of each word in the array ['apple', 'banana', 'cherry'].
  let arr= ['dog', 'cat', 'elephant', 'mouse']
- let f2 = arr.map((e)=>e.length)
- console.log(f2); // output [3,2,8,5]
+ console.log(arr.map((e)=>e.length)); // output [3,2,8,5]
 
 //Q 3 )Using filter(), create a function that filters out the numbers greater than 10 from the array [4, 15, 9, 20, 5].
 const a1 =[4, 15, 9, 20, 5]
-var a2 =a1.filter((e)=>e>10)
-console.log(a2);//[15,20]
+console.log(a1.filter((e)=>e>10));//[15,20]
 
 //Q 4 )Given an array of people with name and age (like the code you provided), use filter() to return a list of people who are 30 years or older.
 const people = [
@@ -52,8 +49,7 @@ const people = [
   {name: 'Jane', age: 40},
   {name: 'Smith', age: 50}
 ];
-const pe=people.filter((e)=>e.age >=30)
-console.log(pe);//[{name: 'Jane', age: 40},{name: 'Smith', age: 50}]
+console.log(people.filter((e)=>e.age>=30));//[{name: 'Jane', age: 40},{name: 'Smith', age: 50}]
 
 //Q 5 )Write a reduce() function that calculates the total age of people in this array:
 const peop = [
@@ -61,13 +57,11 @@ const peop = [
   {name: 'Jane', age: 30},
   {name: 'Smith', age: 45}
 ];
-let m = peop.reduce((acc, b)=>acc+b.age,0)
-console.log(m);//100
+console.log( peop.reduce((acc, b)=>acc+b.age,0));//100
 
 //Q 6 )Use map() to convert the array ['apple', 'banana', 'cherry'] into an array of objects, where each object contains the fruitName and its length.
 let arrr= ['apple', 'banana', 'cherry'] 
- let f5 = arrr.map((e)=>{return `${e} : length of element ${e.length}`})
- console.log(f5);
+console.log(arrr.map((e)=>{return `${e} : length of element ${e.length}`}));
 
  //Q 8 )Use filter() to extract all people from the array who are under 40 and sort the result alphabetically by their name.
 const p = [
@@ -79,8 +73,7 @@ const p = [
   {name: 'imith', age: 38}
 ]
  let a = p.filter((e)=>e.age>40)
- let b = a.sort((a,b)=>a.name.localeCompare(b.name))
-console.log(b);
+console.log(a.sort((a,b)=>a.name.localeCompare(b.name)));
 
 //Q 9 ) Use map() and filter() together to get the sum of the lengths of all words in the array  ['dog', 'cat', 'elephant', 'mouse'], but only for words that have more than 3 characters.
 let arrrr= ['dog', 'cat', 'elephant', 'mouse']
